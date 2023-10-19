53e7bea8f0d4838b4c71b01c75f9285ee81cdf79
Elizabeth Maher <enewman@microsoft.com>
2023-10-19 13:46:53 -0700

    saving work

--- LabServices-V2/hyperv-check-vm/Test-HypervVmConfig.ps1
+++ LabServices-V2/hyperv-check-vm/Test-HypervVmConfig.ps1
@@ -34,24 +34,43 @@ if ($savedStateVMs){
 }
 
 #For each VM
-$vms | ForEach-Object{
-
-    Write-Host "Testing $_.VMName"
+foreach ($vm in $vms){
+    Write-Host "*** Testing $($vm.VMName) ***"
 
     # TODO: Add check to see if VM is running. We won't be able to some change settings if it is.
 
     # Set automatic shutdown action is Shutdown
-    if ($_.AutomaticStopAction -ne "ShutDown"){
-        if ($PSCmdlet.ShouldContinue("Change AutomaticStopOption to ShutDown?  This will require stopping the VM, if it is running.", "AutomaticStopOption for $_.VMName", [ref] $YesToAll, [ref] $NoToAll )){
-            $_ | Stop-VM
-            $_ | Set-VM -AutomaticStopAction ShutDown
+    if ($vm.AutomaticStopAction -ne "ShutDown"){
+        if ($PSCmdlet.ShouldContinue("Change AutomaticStopOption from $($vm.AutomaticStopAction) to ShutDown?  This will require stopping the VM, if it is running.", "AutomaticStopOption for $($vm.VMName)", [ref] $YesToAll, [ref] $NoToAll )){
+            $vm | Stop-VM -Force -WarningAction SilentlyContinue
+            $vm | Set-VM -AutomaticStopAction ShutDown
         }
 
     }
 
     # Verify disk is vhdx not vhd
 
-    # CPUs is more than default 1 CPU
+    # Verify CPUs is more than default 1 CPU
+    if ($vm.ProcessorCount -eq 1){
+        if ($PSCmdlet.ShouldContinue("VM has only has 1 virtual processor assigned to it. Many modern OSes require more.  Increase number of virtual processors for VM? This will require stopping the VM, if it is running.", "Virtual Processor(s) for $($vm.VMName)", [ref] $YesToAll, [ref] $NoToAll )){
+            $number_of_vCPUs = Read-Host "How many cores/virtual processors (max $($env:NUMBER_OF_PROCESSORS))?"
+            $number_of_vCPUs = [math]::Min($number_of_vCPUs, $env:NUMBER_OF_PROCESSORS)
+            $vm | Stop-VM -Force -WarningAction SilentlyContinue
+            $vm | Set-VM -ProcessorCount $number_of_vCPUs
+        }
+    }
 
-    # Memory is more than default 512
+    # Verify Memory is more than default 512
+    $assignedMemory = $vm | Get-VMMemory
+    if ($assignedMemory.Startup -lt 6 * [math]::Pow(10,8)){
+        if ($PSCmdlet.ShouldContinue("VM has only has $($assignedMemory.Startup) MB memory assigned to it assigned to it. Many modern OSes require more. Increase assigned memory to 2GB?`n`nWARNING: This will require stopping the VM, if it is running.", "Assigned Memory for $($vm.VMName)", [ref] $YesToAll, [ref] $NoToAll )){
+            #$input = [int]$(Read-Host "How much memory?")
+            $vm | Stop-VM -Force -WarningAction SilentlyContinue
+            $vm | Set-VMMemory -Startup 2GB
+        }
+    }
+
+
+    #TODO: Check variable memory
+
 }
